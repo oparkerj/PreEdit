80e0ec212767a4c0dc24c938646e02a80f3b8ea1
567legodude <oparkerj@gmail.com>
2018-03-17 23:14:02 -0700

    Input Serialization Added serialization methods to Input for saving/loading.

--- src/main/java/com/ssplugins/preedit/input/Input.java
+++ src/main/java/com/ssplugins/preedit/input/Input.java
@@ -1,29 +1,46 @@
 package com.ssplugins.preedit.input;
 
 import com.ssplugins.preedit.exceptions.InvalidInputException;
-import com.ssplugins.preedit.exceptions.SilentFailException;
 import javafx.scene.Node;
+import javafx.util.StringConverter;
 
 import java.util.Optional;
 
 public abstract class Input<N extends Node, O> {
 	
 	private N node;
+	private StringConverter<O> converter;
 	private boolean ready;
+	private boolean userProvided;
 
 	protected abstract N createInputNode();
 	
 	protected abstract O getNodeValue(N node) throws InvalidInputException;
 	
+	protected abstract void setNodeValue(N node, O value);
+	
 	protected abstract boolean isValid(O value);
+	
+	protected abstract StringConverter<O> getStringConverter();
+	
+	public abstract O getDefaultValue();
 	
 	protected final void ready() {
 		this.ready = true;
 		this.node = createInputNode();
+		this.converter = getStringConverter();
 	}
 	
 	public final boolean isReady() {
 		return ready;
+	}
+	
+	public final void setUserProvided(boolean userProvided) {
+		this.userProvided = userProvided;
+	}
+	
+	public final boolean isUserProvided() {
+		return userProvided;
 	}
 	
 	public final <T extends Input> Optional<T> as(Class<T> type) {
@@ -43,6 +60,14 @@ public abstract class Input<N extends Node, O> {
 		} catch (InvalidInputException e) {
 			return Optional.empty();
 		}
+	}
+	
+	public final String serialize() {
+		return getValue().map(converter::toString).orElse(null);
+	}
+	
+	public final void deserialize(String value) {
+		setNodeValue(node, converter.fromString(value));
 	}
 	
 	public final boolean isValid() {
