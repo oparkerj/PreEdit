da9b80867b19664fb2d37a42a4887cc50decfb46
567legodude <oparkerj@gmail.com>
2018-03-18 18:36:43 -0700

    Color Picker Added a color picker as a form of input.

---
+++ src/main/java/com/ssplugins/preedit/input/ColorInput.java
@@ -0,0 +1,60 @@
+package com.ssplugins.preedit.input;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.ssplugins.preedit.exceptions.InvalidInputException;
+import com.ssplugins.preedit.util.JsonConverter;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.paint.Color;
+
+public class ColorInput extends Input<ColorPicker, Color> {
+	
+	public ColorInput() {
+		this.ready();
+	}
+	
+	@Override
+	protected ColorPicker createInputNode() {
+		return new ColorPicker();
+	}
+	
+	@Override
+	protected Color getNodeValue(ColorPicker node) {
+		return node.getValue();
+	}
+	
+	@Override
+	protected void setNodeValue(ColorPicker node, Color value) {
+		node.setValue(value);
+	}
+	
+	@Override
+	protected boolean isValid(Color value) {
+		return true;
+	}
+	
+	@Override
+	protected JsonConverter<Color> getJsonConverter() {
+		return new JsonConverter<Color>() {
+			@Override
+			public JsonElement toJson(Color color) {
+				JsonArray arr = new JsonArray();
+				arr.add(color.getRed());
+				arr.add(color.getGreen());
+				arr.add(color.getBlue());
+				arr.add(color.getOpacity());
+				return arr;
+			}
+			
+			@Override
+			public Color fromJson(JsonElement element) {
+				JsonArray json = element.getAsJsonArray();
+				return Color.color(json.get(0).getAsDouble(),
+								   json.get(1).getAsDouble(),
+								   json.get(2).getAsDouble(),
+								   json.get(3).getAsDouble());
+			}
+		};
+	}
+	
+}
