cda634b81b08239ae2c25242dd06e0b9ce69d01a
567legodude <oparkerj@gmail.com>
2018-05-10 15:43:57 -0700

    Update API Added some relevant values into the API.

--- src/main/java/com/ssplugins/preedit/api/PreEditAPI.java
+++ src/main/java/com/ssplugins/preedit/api/PreEditAPI.java
@@ -1,9 +1,12 @@
 package com.ssplugins.preedit.api;
 
+import com.ssplugins.preedit.edit.Catalog;
 import com.ssplugins.preedit.gui.GUI;
 
 public interface PreEditAPI {
     
     GUI getGUI();
+    
+    Catalog getCatalog();
     
 }
--- src/main/java/com/ssplugins/preedit/api/PreEditTab.java
+++ src/main/java/com/ssplugins/preedit/api/PreEditTab.java
@@ -1,12 +1,20 @@
 package com.ssplugins.preedit.api;
 
+import javafx.beans.binding.BooleanBinding;
 import javafx.scene.Node;
 import javafx.scene.control.ToolBar;
+
+import java.awt.image.BufferedImage;
+import java.util.Optional;
 
 public interface PreEditTab {
     
     ToolBar getToolbar();
     
     void addToolbarNode(Node node);
+    
+    BooleanBinding templateLoadedProperty();
+    
+    Optional<BufferedImage> renderImage();
     
 }
