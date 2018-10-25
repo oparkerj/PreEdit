0a41d9f88803bda2f7fa177d3bb6fcefc2c7cb57
StarShadow <oparkerj@gmail.com>
2018-10-24 20:40:31 -0700

    UndoHistory update Working on getting undo history working.

--- PreEdit/src/main/java/com/ssplugins/preedit/util/UndoHistory.java
+++ PreEdit/src/main/java/com/ssplugins/preedit/util/UndoHistory.java
@@ -8,31 +8,49 @@ import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class UndoHistory {
     
     private int capacity = 200;
     private int pointer = -1;
-    private boolean working;
+    private AtomicBoolean working;
     
-    private List<UndoStep<?>> list = new ArrayList<>();
+    private List<UndoTrigger> triggers = new ArrayList<>();
+    private List<Undo> list = new ArrayList<>();
     
     private boolean mouseDown;
     private List<KeyCode> keys = new ArrayList<>();
     
     public UndoHistory(Stage stage) {
+        working = new AtomicBoolean();
         stage.addEventFilter(MouseEvent.MOUSE_PRESSED, event -> {
             mouseDown = true;
         });
         stage.addEventFilter(MouseEvent.MOUSE_RELEASED, event -> {
             mouseDown = false;
+            collectSteps();
         });
         stage.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
             keys.add(event.getCode());
         });
         stage.addEventFilter(KeyEvent.KEY_RELEASED, event -> {
             keys.remove(event.getCode());
+            collectSteps();
         });
+    }
+    
+    private void collectSteps() {
+        List<Undo> steps = new ArrayList<>();
+        triggers.stream().map(UndoTrigger::getPendingChange).filter(Optional::isPresent).map(Optional::get).forEach(steps::add);
+        if (steps.size() == 1) {
+            push(steps.get(0));
+        }
+        else {
+            push(new UndoGroup(steps));
+        }
     }
     
     public boolean isButtonHeld() {
@@ -44,7 +62,7 @@ public class UndoHistory {
     }
     
     public boolean isWorking() {
-        return working; // todo this
+        return working.get();
     }
     
     public int getCapacity() {
@@ -58,49 +76,51 @@ public class UndoHistory {
         }
     }
     
-    public <T> UndoTrigger<T> createTrigger() {
-        return new UndoTrigger<>(this);
+    public UndoTrigger createTrigger() {
+        UndoTrigger trigger = new UndoTrigger(this);
+        triggers.add(trigger);
+        return trigger;
     }
     
     public void undo() {
         synchronized (this) {
             if (list.size() == 0) return;
-            if (working) return;
-            working = true;
+            if (working.get()) return;
+            working.set(true);
             boolean push = false;
             if (pointer == -1) {
                 push = true;
                 pointer = list.size();
             }
-            else pointer--;
+            pointer--;
             if (pointer < 0) {
                 pointer = 0;
                 return;
             }
-            UndoStep<?> step = list.get(pointer);
-            if (push) list.add(step.currentStep());
+            Undo step = list.get(pointer);
+            if (push) list.add(step.redoStep());
             step.restore();
-            working = false;
+            working.set(false);
         }
     }
     
     public void redo() {
         synchronized (this) {
-            if (working) return;
-            working = true;
+            if (working.get()) return;
+            working.set(true);
             if (pointer == -1) return;
             pointer++;
             if (pointer == list.size()) {
                 pointer--;
                 return;
             }
-            UndoStep<?> step = list.get(pointer);
+            Undo step = list.get(pointer);
             step.restore();
-            working = false;
+            working.set(false);
         }
     }
     
-    private void push(UndoStep<?> step) {
+    private void push(Undo step) {
         if (pointer > -1) {
             pointer++;
             while (list.size() > pointer) list.remove(pointer);
@@ -110,7 +130,15 @@ public class UndoHistory {
         while (list.size() > capacity) list.remove(0);
     }
     
-    private class UndoStep<T> {
+    private abstract class Undo {
+        
+        public abstract void restore();
+        
+        public abstract Undo redoStep();
+        
+    }
+    
+    private class UndoStep<T> extends Undo {
         
         private Property<T> property;
         private T oldValue;
@@ -120,62 +148,69 @@ public class UndoHistory {
             this.oldValue = oldValue;
         }
         
-        private UndoStep<T> currentStep() {
+        @Override
+        public Undo redoStep() {
             return new UndoStep<>(property, property.getValue());
         }
         
-        private void restore() {
+        @Override
+        public void restore() {
             property.setValue(oldValue);
         }
         
     }
     
-    public class UndoTrigger<T> {
+    private class UndoGroup extends Undo {
+    
+        private List<Undo> steps;
+    
+        public UndoGroup() {
+            steps = new ArrayList<>();
+        }
+    
+        public UndoGroup(List<Undo> steps) {
+            this.steps = steps;
+        }
+    
+        public void addStep(Undo step) {
+            steps.add(step);
+        }
+    
+        @Override
+        public Undo redoStep() {
+            UndoGroup redo = new UndoGroup();
+            steps.forEach(step -> redo.addStep(step.redoStep()));
+            return redo;
+        }
+    
+        @Override
+        public void restore() {
+            steps.forEach(Undo::restore);
+        }
+        
+    }
+    
+    public class UndoTrigger {
     
         private UndoHistory history;
-        private T data;
-        
-        // Custom settings
-        private long last;
-        private long interval;
-        private boolean bypass;
-        private boolean heldUsed;
+    
+        private AtomicReference<Undo> submit = new AtomicReference<>();
     
         public UndoTrigger(UndoHistory history) {
             this.history = history;
         }
-    
-        public T getData() {
-            return data;
-        }
-    
-        public void setData(T data) {
-            this.data = data;
-        }
-    
-        public long getInterval() {
-            return interval;
-        }
-    
-        public void setInterval(long interval) {
-            this.interval = interval;
-        }
         
-        public void bypassHold() {
-            bypass = true;
+        private Optional<Undo> getPendingChange() {
+            Undo step = submit.get();
+            submit.set(null);
+            return Optional.ofNullable(step);
         }
     
         public <U> void submit(Property<U> property, U oldValue) {
             if (history.isWorking()) return;
-            if (history.isButtonHeld() && !bypass) {
-                if (heldUsed) return;
-                heldUsed = true;
-            }
-            else heldUsed = false;
-            if (interval > 0) {
-                long d = System.currentTimeMillis() - last;
-                if (d <= interval) return;
-                last = System.currentTimeMillis();
+            if (history.isButtonHeld()) {
+                submit.updateAndGet(undoStep -> new UndoStep<>(property, oldValue));
+                return;
             }
             history.push(new UndoStep<>(property, oldValue));
         }
