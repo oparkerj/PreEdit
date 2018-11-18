a2c02999fbbf9f39c2d84f37126656c6b0c27ae1
StarShadow <oparkerj@gmail.com>
2018-11-17 22:04:53 -0700

    Cleanup Removed old test effects.

--- PreEdit/src/main/java/com/ssplugins/preedit/PreEdit.java
+++ PreEdit/src/main/java/com/ssplugins/preedit/PreEdit.java
@@ -78,10 +78,6 @@ public class PreEdit extends Application implements PreEditAPI {
         catalog.registerEffect("MotionBlur", MotionBlurEffect.class);
         catalog.registerEffect("Reflection", ReflectionEffect.class);
         catalog.registerEffect("Clip", ClipEffect.class);
-        
-        catalog.registerEffect("Scale", ScaleEffect.class);
-        
-//        catalog.registerEffect("BoundingBox", BoundingBoxEffect.class);
     }
     
     private List<String> loadAddons() {
